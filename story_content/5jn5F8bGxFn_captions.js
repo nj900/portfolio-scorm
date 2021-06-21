80c05df63a84dd132ad46d24fd7f0e83e83e9973
nj900 <71087293+nj900@users.noreply.github.com>
2021-06-21 16:08:43 -0400

    Add files via upload

---
+++ story_content/5jn5F8bGxFn_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.38.21861.0%0D%0A%0D%0A00:00:00.088%20--%3E%2000:00:02.956%0D%0ALet%E2%80%99s%20try%20one%20last%20time%20in%20period%205.%0D%0A%0D%0A"}')
